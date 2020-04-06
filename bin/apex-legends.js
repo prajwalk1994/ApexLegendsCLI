18413dbda9bffb9ff02c7ca410cfdc8b10399e3b
prajwal <kondubhatlav.eprajwal@gmail.com>
2020-04-06 00:43:24 -0700

    Add stat to root commander

--- bin/apex-legends.js
+++ bin/apex-legends.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 const pkg = require("../package.json");
 const program = require("commander");
+const stat = require("../actions/stat");
 
 program
   .version(pkg.version)
-  .command("key", "Manage Key -- Get key at https://tracker.gg/developers")
-  .parse(process.argv);
+  .command("key", "Manage Key -- Get key at https://tracker.gg/developers");
+
+program
+  .command("stat")
+  .description("Check the Stats of Each Player")
+  .option("-P <type>", "Enter the player tag of the platform")
+  .option("--platform <type>", "Platform of the Player (PSN, Origin, Xbox)")
+  .action((opts) => stat.show(opts));
+
+program.parse(process.argv);
